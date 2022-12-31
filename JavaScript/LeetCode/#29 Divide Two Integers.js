400453c9f338de272e3a5951a94dfc7834dbc62e
SeungHeeBae <66785642+b-sseung@users.noreply.github.com>
2022-12-31 15:01:59 +0900

    Create #29 Divide Two Integers.js

---
+++ JavaScript/LeetCode/#29 Divide Two Integers.js
@@ -0,0 +1,8 @@
+var divide = function(dividend, divisor) {
+    let result = parseInt(dividend / divisor);
+    
+    if (result > Math.pow(2, 31) - 1) return Math.pow(2, 31) - 1;
+    if (result < Math.pow(2, 31) * -1) return Math.pow(2, 31) * -1;
+
+    return result;
+};
